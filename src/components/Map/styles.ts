b5eebb9578a194ec29778e288925eb3d7ff32c43
Rodrigo Candido <rodrigocan@gmail.com>
2021-03-18 12:57:35 -0300

    Aula 49

---
+++ src/components/Map/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const MapWraper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .leaflet-container {
+    background-color: var(--background);
+  }
+`
